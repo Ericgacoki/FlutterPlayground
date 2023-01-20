5073379b895318086e4ba2910268854bc7dac609
Ericgacoki <gacokieric@gmail.com>
2023-01-20 19:42:06 +0300

    navigation

---
+++ lib/page2.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatefulWidget {
+  const Page2({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Page2> createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Text('This is page 2'),
+      ),
+    );
+  }
+}
